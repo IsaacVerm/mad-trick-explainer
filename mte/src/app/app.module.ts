284090164965fa892898cc12ef19f25f5f29e1eb
Isaac Verminck <isaacverm@gmail.com>
2020-05-21 21:44:53 +0200

    install material design

--- mte/src/app/app.module.ts
+++ mte/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MagicianListComponent } from './magician-list/magician-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MagicianListComponent } from './magician-list/magician-list.component';
     BrowserModule,
     RouterModule.forRoot([
       { path: 'magician-list', component: MagicianListComponent },
-    ])
+    ]),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
