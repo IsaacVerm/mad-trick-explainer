c330f815abe20dfdfcc9945592b1cbc5c4320cfd
Isaac Verminck <isaacverm@gmail.com>
2020-05-22 15:01:39 +0200

    update magician-details so it has access to id in url

--- mte/src/app/magician-details/magician-details.component.ts
+++ mte/src/app/magician-details/magician-details.component.ts
@@ -1,16 +1,20 @@
+import { magicianNames } from './../magicians';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-magician-details',
   templateUrl: './magician-details.component.html',
-  styleUrls: ['./magician-details.component.css']
+  styleUrls: ['./magician-details.component.css'],
 })
 export class MagicianDetailsComponent implements OnInit {
+  magician;
 
-  constructor(
-  ) { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.magician = magicianNames[+params.get('magicianId')];
+    })
   }
-
 }
