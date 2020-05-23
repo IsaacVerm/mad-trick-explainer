9f3cc052af26673cc33a151de509589c16fbe29f
Isaac Verminck <isaacverm@gmail.com>
2020-05-23 17:40:31 +0200

    use ConfigService in MagiciansService

--- mte/src/app/magicians.service.ts
+++ mte/src/app/magicians.service.ts
@@ -1,3 +1,5 @@
+import { ConfigService } from './config.service';
+import { Config } from './config.service'
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
@@ -12,10 +14,14 @@ export interface MagicianNames {
 })
 export class MagiciansService {
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
+    private config: ConfigService
   ) { }
 
   getMagicianNames() {
+    this.config.getConfig().subscribe((config: Config) => {
+      console.log(config.apiUrl);
+    });
     return this.http.get<MagicianNames>('https://127.0.0.1:5000/names');
   }
 }
