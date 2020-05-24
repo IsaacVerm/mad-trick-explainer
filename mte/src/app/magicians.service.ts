efe4b884e26b01e49c3cd2c850e47c3abc379ca5
Isaac Verminck <isaacverm@gmail.com>
2020-05-24 17:34:06 +0200

    remove unused rxjs imports MagiciansService

--- mte/src/app/magicians.service.ts
+++ mte/src/app/magicians.service.ts
@@ -2,8 +2,6 @@ import { ConfigService } from './config.service';
 import { Config } from './config.service'
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 
 export interface MagicianNames {
   names: string[];
