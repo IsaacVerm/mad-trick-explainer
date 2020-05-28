2af0290dd18013519d2419c01bfd3730569964cd
Isaac Verminck <isaacverm@gmail.com>
2020-05-28 10:38:35 +0200

    use ngOnDestroy to unsubscribe from MagiciansService

--- mte/src/app/magician-list/magician-list.component.ts
+++ mte/src/app/magician-list/magician-list.component.ts
@@ -1,25 +1,29 @@
 import { MagiciansService } from './../magicians.service';
 import { MagicianNames } from './../magicians.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-magician-list',
   templateUrl: './magician-list.component.html',
   styleUrls: ['./magician-list.component.css']
 })
-export class MagicianListComponent implements OnInit {
+export class MagicianListComponent implements OnInit, OnDestroy {
   magicianNames;
+  subscription;
 
   constructor(
     private magiciansService: MagiciansService
     ) {}
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   ngOnInit(): void {
     this.getMagicianNames();
   }
 
   getMagicianNames() {
-    this.magiciansService
+    this.subscription = this.magiciansService
       .getMagicianNames()
       .subscribe((response: MagicianNames) => {
         this.magicianNames = response.names;
--- mte/src/app/magicians.service.ts
+++ mte/src/app/magicians.service.ts
@@ -3,7 +3,7 @@ import { ConfigService } from './config.service';
 import { Config } from './config.service'
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { switchMap } from 'rxjs/operators';
 
 export interface MagicianNames {
   names: string[];
@@ -22,20 +22,13 @@ export class MagiciansService {
     // getConfig() returns an observable
     // so you have to subscribe to it
     // https://medium.com/@luukgruijs/understanding-creating-and-subscribing-to-observables-in-angular-426dbf0b04a3
-
-
     // https://stackoverflow.com/questions/49630371/return-a-observable-from-a-subscription-with-rxjs
     // map should return Observable
     return this.config.getConfig().pipe(
-      map(config => {
+      switchMap(config => {
         const apiUrl = config.apiUrl;
         return this.http.get<MagicianNames>(`${apiUrl}/names`);
       })
     );
-
-    // this.config.getConfig().subscribe((config) => {
-    //   const apiUrl = config.apiUrl;
-    //   console.log(this.http.get<MagicianNames>(`${apiUrl}/names`));
-    // });
   }
 }
