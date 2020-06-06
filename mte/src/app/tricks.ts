590a4a562b02543a6b968abf7d634345ebe2aabc
Isaac Verminck <isaacverm@gmail.com>
2020-06-06 10:36:04 +0200

    add tricks demo data

---
+++ mte/src/app/tricks.ts
@@ -0,0 +1,38 @@
+export const tricks = [
+         {
+           name: 'Bullet catch',
+           inventor: 'Jean Chassanion',
+           type: 'stage magic illusion',
+           description:
+             'a magician appears to catch a bullet fired directly at him or her ',
+           synonyms: ['bullet trick', 'defying the bullets', 'gun trick'],
+           wikipediaLink: 'https://en.wikipedia.org/wiki/Bullet_catch',
+         },
+         {
+           name: 'Drill of Death',
+           inventor: 'André Kole',
+           type: 'stage magic illusion',
+           description: 'a performer appears to be impaled on a giant drill',
+           synonyms: [],
+           wikipediaLink: 'https://en.wikipedia.org/wiki/Drill_of_Death',
+         },
+         {
+           name: 'Floating match on card',
+           inventor: 'Ben Harris',
+           type: 'close-up effect',
+           description:
+             'a magician causes the match to appear to float off the playing card.',
+           synonyms: ['Cosmosis'],
+           wikipediaLink:
+             'https://en.wikipedia.org/wiki/Floating_match_on_card',
+         },
+         {
+           name: 'Three-card Monte',
+           inventor: 'anonymous',
+           type: 'confidence game',
+           description:
+             'victim gets reward if money card found among three face-down playing cards',
+           synonyms: ['Find the Lady', 'Three-card Trick'],
+           wikipediaLink: 'https://en.wikipedia.org/wiki/Three-card_Monte',
+         },
+       ];
